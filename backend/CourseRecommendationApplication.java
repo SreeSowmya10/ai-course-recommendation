a38801e61952d95b803343532ca6132018b5414f
SreeSowmya10 <SGaddam56@students.cumberland.edu>
2026-04-19 21:25:23 +0530

    Create CourseRecommendationApplication.java

---
+++ backend/CourseRecommendationApplication.java
@@ -0,0 +1,11 @@
+package com.example.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRecommendationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRecommendationApplication.class, args);
+    }
+}
